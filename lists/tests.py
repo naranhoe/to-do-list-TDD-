a4f5d846544ed7e17fb1d4ad4fe33b3446c11aa3
naranhoe <j.naranjojr@gmail.com>
2018-03-18 15:42:50 -0400

    Cleaned up, tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,4 @@
-from django.template.loader import render_to_string
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
